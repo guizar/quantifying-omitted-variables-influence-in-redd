66517bc5ed08abeffd0a3d03346d1698a99d6267
Alejandro Guizar <guizarcou@gmail.com>
2025-10-22 14:55:26 +0100

    sensitivity plots WIP

--- code/06a_sensitivity_analysis_with_panel.R
+++ code/06a_sensitivity_analysis_with_panel.R
@@ -43,7 +43,7 @@ res_plot <- res_plot2 <- tibble()
 
 # List of observed confounders for sensitivity analysis
 var_benchmark <- list(
-  dist_degra = "dist_degra",
+  dist_degra = "dist_degra"
 )
 var_benchmark_plot_order <- c("none", "dist_degra")
 
@@ -104,14 +104,14 @@ xvars = c("dist_degra" )
 cluster_choice = c("gid_fac", "time_treat_fac")
 fixefs = c("gid_fac", "time_treat_fac")
 
-m_sub <- panel_data_fit_one_project(panel_sub,
+m_matched <- panel_data_fit_one_project(panel_sub,
                                          y = "z",
                                          xvars = xvars,
                                          fixefs = fixefs,
                                          cluster = cluster_choice,
                                           offset = "offset_yr")
 
-m <- panel_data_fit_one_project(d_unmatched,
+m_unmatched <- panel_data_fit_one_project(d_unmatched,
                                          y = "z",
                                          xvars = xvars,
                                          fixefs = fixefs,
@@ -119,6 +119,200 @@ m <- panel_data_fit_one_project(d_unmatched,
                                           offset = 
                                           "offset_yr")     
 
-unmatched_coef_tab <- summary(m)$coefficients
+unmatched_coef_tab <- summary(m_unmatched)$coefficients
 all_vars_available_to_benchmark <- names(unmatched_coef_tab)
+
+  res <- tibble()
+  
+  # Sensitivity analysis using benchmark variables and strength of hidden confounders
+  for (k_try_curr in k_try) {
+    for (vars_names_curr in names(var_benchmark)) {
+      vars_use_curr <- intersect(var_benchmark[[vars_names_curr]], names(unmatched_coef_tab))
+      
+      if (length(vars_use_curr) > 0) {
+        try_error <- try(silent = TRUE, expr = {
+          bounds_out <- sensemakr:::ovb_bounds.fixest(
+            model = m_unmatched,
+            treatment = "treat:post",
+            benchmark_covariates = list(var_cur = vars_use_curr),
+            kd = k_try_curr,
+            ky = k_try_curr
+          )
+          r2yz.dx_use <- bounds_out$r2yz.dx
+          r2dz.x_use <- bounds_out$r2dz.x
+        })
+        
+        if (inherits(try_error, "try-error")) {
+          add <- tibble(
+            proj_id = proj,
+            covariate = vars_names_curr,
+            k = k_try_curr,
+            adjusted_est_offset = -Inf,
+            claimed_ate = ate_yr_to_compare,
+            adjusted_est_no_offset = -Inf,
+            adjusted_ci_low = -Inf,
+            adjusted_ci_upp = -Inf
+          )
+        } else {
+          est_curr_reduce <- sensemakr:::adjusted_estimate.fixest(
+            model = m_matched,
+            treatment = "treat:post",
+            r2yz.dx = r2yz.dx_use,
+            r2dz.x = r2dz.x_use,
+            reduce = TRUE
+          )
+          est_curr_increase <- sensemakr:::adjusted_estimate.fixest(
+            model = m_matched,
+            treatment = "treat:post",
+            r2yz.dx = r2yz.dx_use,
+            r2dz.x = r2dz.x_use,
+            reduce = FALSE
+          )
+          est_curr <- min(c(est_curr_reduce, est_curr_increase))
+          se_curr <- sensemakr:::adjusted_se.fixest(
+            model = m_matched,
+            treatment = "treat:post",
+            r2yz.dx = r2yz.dx_use,
+            r2dz.x = r2dz.x_use
+          )
+          
+          # adapted as fixest doesn't provide a direct way to call DF
+          df_res <- m_matched$nobs - m_matched$nparams
+          ci_mult <- qt(p = 0.975, df = df_res)
+          
+          add <- tibble(
+            proj_id = proj,
+            covariate = vars_names_curr,
+            k = k_try_curr,
+            adjusted_est_offset = est_curr,
+            claimed_ate = ate_yr_to_compare,
+            adjusted_est_no_offset = est_curr + ate_yr_to_compare,
+            adjusted_ci_low = adjusted_est_no_offset - ci_mult * se_curr,
+            adjusted_ci_upp = adjusted_est_no_offset + ci_mult * se_curr,
+            r2yz_dx = r2yz.dx_use,
+            r2dz_x = r2dz.x_use
+          )
+        }
+        
+        if (k_try_curr == 0) {
+          add$covariate <- "none"
+          if (vars_names_curr == names(var_benchmark)[1]) {
+            res <- res %>% bind_rows(add)
+          }
+        } else {
+          res <- res %>% bind_rows(add)
+        }
+      }
+    }
+  }
+  
+  # Filter and arrange results for plotting
+  res_add <- res %>%
+    filter(k != 0) %>%
+    group_by(k) %>%
+    mutate(min_adjusted_ci_low = min(adjusted_ci_low)) %>%
+    filter(adjusted_ci_low == min_adjusted_ci_low) %>%
+    ungroup() %>%
+    arrange(k)
+  
+  res_add <- res_add %>%
+    bind_rows(res %>% filter(k == 0 & covariate == res_add$covariate[1])) %>%
+    arrange(k)
+  
+  res_plot <- res_plot %>% bind_rows(res_add)
+  res_plot2 <- res_plot2 %>% bind_rows(res)
 }
+
+# Plot settings and preparation for output
+min_y <- -8
+claimed_ate_thresh <- -7.5
+order_proj <- proj_tab %>%
+  arrange(-implied_ate) %>%
+  pull(proj_id)
+
+res_plot_filt2 <- res_plot2 %>%
+  mutate(k = as.character(k)) %>%
+  mutate(mn_plot = adjusted_est_no_offset) %>% # ifelse(adjusted_est_no_offset < min_y, min_y, adjusted_est_no_offset)) %>%
+  mutate(proj_id = factor(proj_id, levels = order_proj)) %>%
+  mutate(Covariate = factor(covariate, levels = var_benchmark_plot_order)) %>%
+  mutate(out_of_scale = as.numeric(mn_plot <= min_y)) %>%
+  mutate(`Point type` = factor(ifelse(mn_plot <= min_y, paste0("y < ", min_y), "y value plotted"), levels = c("y value plotted", paste0("y < ", min_y))))
+
+# Edit labels
+res_plot_filt2 <- res_plot_filt2  %>% 
+  mutate(Covariate = factor(Covariate, labels=c("None", "Distance to degradation")))
+
+# -----------------------------------
+# Fig 6 - sensitivity plots
+# -----------------------------------
+
+df_plot_sensitivity_sub <- res_plot_filt2 %>%
+  filter(!proj_id %in% projects_with_low_matched_prop) %>%
+  mutate(is_larger = mn_plot < claimed_ate) %>%
+  group_by(k, Covariate) %>%
+  summarise(n = n(), is_larger = sum(is_larger), .groups = "drop") %>%
+  mutate(perc_is_larger = 100 * is_larger / n)
+
+col_palette <- rev(carto_pal(5, "ag_Sunset"))
+col_palette[1] <- "#B7B7B7"
+
+# Set factor levels to control order of x-axis
+df_plot_sensitivity_sub <- df_plot_sensitivity_sub %>%
+  mutate(k_label = factor(paste0(k, "x"), levels = paste0(0:3, "x")))
+
+# Plot 1
+plt_1 <- ggplot(
+  df_plot_sensitivity_sub,
+  aes(x = k_label, y = perc_is_larger, fill = Covariate, color = Covariate)
+) +
+  geom_bar(stat = "identity", position = position_dodge2(width = 0.9, preserve = "single"), width = 0.8) +
+  scale_color_manual(values = col_palette, name = "") +
+  scale_fill_manual(values = col_palette, name = "") +
+  xlab("Strength of hidden confounder\n(x times strength of observed covariate)") +
+  ylab("Projects exceeding\nVCS claimed ATE (%)") +
+  theme_bw() +
+  theme(
+    strip.background = element_blank(),
+    strip.text = element_text(face = "bold"),
+    legend.position = "none"
+  ) +
+  ylim(0, 100)
+
+# Plot 2
+pl_out <- ggplot(
+  res_plot_filt2 %>%
+    filter(!proj_id %in% projects_with_low_matched_prop) %>%
+    mutate(k_label = factor(paste0(k, "x"), levels = paste0(0:3, "x"))),
+  aes(x = k_label, y = mn_plot, fill = Covariate, color = Covariate)
+) +
+  geom_point(position = position_dodge(width = 0.5), size = 2) +
+  geom_errorbar(aes(ymin = adjusted_ci_low, ymax = adjusted_ci_upp), width = 0, position = position_dodge(width = 0.5)) +
+  geom_hline(aes(yintercept = claimed_ate), colour = "red", linetype = "dashed") +
+  geom_hline(yintercept = 0, colour = "black") +
+  scale_color_manual(values = col_palette, name = "") +
+  scale_fill_manual(values = col_palette, name = "") +
+  xlab("Strength of hidden confounder\n(x times strength of observed covariate)") +
+  ylab(expression("Difference in forest loss (% " * yr^{-1} * ")")) +
+  theme_bw() +
+  facet_wrap(~proj_id, ncol = 5, scales = "free_y") +
+  theme(
+    strip.background = element_blank(),
+    strip.text = element_text(face = "bold"),
+    plot.title = element_text(size = 10, face = "bold", hjust = 0.5),
+    legend.position = "bottom",
+    plot.margin = unit(c(0.01, 0.01, 0.01, 0.01), "in"),
+    legend.box.spacing = unit(0.001, "in"),
+    axis.text.x.top = element_blank(),
+    axis.ticks.x.top = element_blank()
+  )
+
+# Combine
+gg_out <- plt_1 / pl_out +
+  plot_layout(heights = c(0.16, 0.84)) &
+  plot_annotation(tag_levels = "a")
+
+ggsave(
+  filename = file.path(dir_figures, "sensitivity_analysis_panel_main.png"),
+  plot = gg_out,
+  width = 12, height = 12, units = "in", dpi = 300
+)
